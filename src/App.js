35af4ca2e119d1672cfbce1c5d9c80fa4ee4ca3f
Seunghoon <sj00046@mix.wvu.edu>
2024-02-29 09:52:55 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 002</h2>
+      <p>WVU ID: 800439145</p>
+      <p>Hi I am Seunghoon.</p>
+    </div>
+  );
+}
+
+export default App;
